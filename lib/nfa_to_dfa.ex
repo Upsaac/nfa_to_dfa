2069542e9da04981d4515c466a4be2691380ff7a
Dylan Isaac Mancilla <isaac_mancilla@MacBook-Air-de-Dylan.local>
2026-04-08 20:55:31 -0600

    Estructura de datos

--- lib/nfa_to_dfa.ex
+++ lib/nfa_to_dfa.ex
@@ -1,12 +1,24 @@
 defmodule NfaToDfa do
+  defstruct states: [], alphabet: [], transitions: %{}, start_state: nil, final_states: []
+
   def nfa_example do
-  transiciones = [
-    {:inicio, "a", :nodo_a},
-    {:inicio, "b", :nodo_b},
-    {:nodo_a, "c", :fin}
-  ]
+    %NfaToDfa{
+      states: [:inicio, :nodo_a, :nodo_b, :fin],
+      alphabet: ["a", "b", "c"],
+      start_state: :inicio,
+      final_states: [:fin],
+
+      transitions: %{
+        {:inicio, "a"} => [:nodo_a],
+        {:inicio, "b"} => [:nodo_b],
+        {:nodo_a, "c"} => [:fin]
+      }
+    }
   end
 
+  def determinize() do
+
+  end
   def map(), do: 1+2
 
 end
