0bf5c62b622adef6870af17ac238a60c16bce34e
Dylan Isaac Mancilla <isaac_mancilla@MacBook-Air-de-Dylan.local>
2026-04-08 20:35:46 -0600

    Primer commit

---
+++ lib/nfa_to_dfa.ex
@@ -0,0 +1,18 @@
+defmodule NfaToDfa do
+  @moduledoc """
+  Documentation for `NfaToDfa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NfaToDfa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nfa_to_dfa_test.exs
@@ -0,0 +1,8 @@
+defmodule NfaToDfaTest do
+  use ExUnit.Case
+  doctest NfaToDfa
+
+  test "greets the world" do
+    assert NfaToDfa.hello() == :world
+  end
+end
